a861ea0d0dedf806e87cc4118c7f499701e18e96
Nate Garrett <nate.garrett1992@gmail.com>
2017-12-27 19:57:27 -0600

    got math calculations working for current location

---
+++ calc.py
@@ -0,0 +1,21 @@
+from math import sin, cos, sqrt, atan2, radians
+
+
+def calc_distance(lat1, lat2, lon1, lon2):
+    # approximate radius of earth in km
+    R = 6400.0
+
+    rlat1 = radians(lat1)
+    rlon1 = radians(lon1)
+    rlat2 = radians(float(lat2))
+    rlon2 = radians(float(lon2))
+
+    dlon = rlon2 - rlon1
+    dlat = rlat2 - rlat1
+
+    a = sin(dlat / 2)**2 + cos(rlat1) * cos(rlat2) * sin(dlon / 2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+
+    return distance
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import requests
+from calc import calc_distance
 
-info = requests.get('http://api.open-notify.org/iss-now.json').json()
-print('The ISS\'s Position is: ',
-      info['iss_position']['latitude'],
-      info['iss_position']['longitude'])
+# ISS Location pulled from API
+issInfo = requests.get('http://api.open-notify.org/iss-now.json').json()
+issLat = issInfo['iss_position']['latitude']
+issLon = issInfo['iss_position']['longitude']
+issLocation = float(issLat), float(issLon)
 
+# User's Current Location Based on IP
 myPosInfo = requests.get('http://ip-api.com/json').json()
-print('Your location is: ', myPosInfo['lat'], myPosInfo['lon'])
+myLocationLat = float(myPosInfo['lat'])
+myLocationLon = float(myPosInfo['lon'])
+myLocation = myLocationLat, myLocationLon
+
+# Print Locations
+print('The ISS\'s Position is: ', issLocation)
+print('Your location is: ', myLocation)
+
+# Calculate distance of ISS from my location
+distance = calc_distance(myLocationLat, issLat, myLocationLon, issLon)
+print('The ISS is', round(distance, 2), 'km', 'from your current location')
